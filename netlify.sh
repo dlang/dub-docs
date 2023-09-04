6654216d846d1351f31d574c0434978655cae690
WebFreak001 <gh@webfreak.org>
2023-09-04 22:18:51 +0200

    readd D installation to netlify.sh

--- netlify.sh
+++ netlify.sh
@@ -4,7 +4,12 @@
 set -euo pipefail
 set -x
 
+DMD_VERSION="2.105.0"
 BUILD_DIR="out"
+
+CURL_FLAGS=(-fsSL --retry 10 --retry-delay 30 --retry-max-time 600 --connect-timeout 5 --speed-time 30 --speed-limit 1024)
+
+. "$(curl "${CURL_FLAGS[@]}" https://dlang.org/install.sh | bash -s install "dmd-${DMD_VERSION}" --activate)"
 
 ./dub/scripts/man/gen_man.d
 rm -rf docs/cli-reference/
